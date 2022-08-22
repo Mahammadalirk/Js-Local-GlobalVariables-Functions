6d25bc89ac4655acebbb09ddcd54c170444998c8
mahammadalirk <mahammadalirk@code.edu.az>
2022-08-22 07:41:25 +0400

    JS

---
+++ Js-Local, Global variables, Functions/assests/script.js
@@ -0,0 +1,104 @@
+// let k = 21;
+
+// divided(k)
+// function divided(d) {
+//     if (d %3 == 0 && d % 7 == 0) {
+//         console.log("Divided");  
+//     }else{
+//         console.log("not divided");
+//     }
+
+// }
+
+
+
+
+
+// let m = 25;
+
+// factorial(m);
+
+// function factorial(a) {
+//     let factor = 1;
+
+//     for (let i = 1; i <= a; i++) {
+//         factor *= i;
+
+//     }
+//     console.log(factor);
+// }
+
+
+
+
+// let nums = [4,7,1,9,7,4,2,3,8];
+
+// evenSquareOfNums(nums);
+
+// function evenSquareOfNums(square) {
+//     let squ = 0;
+
+//     for (let i = 0; i < square.length; i++) {
+//         if (square[i] % 2 == 0) {
+//             squ += Math.pow(square[i], 2);
+//         }
+
+//     }
+//     console.log(squ);
+
+// }
+
+
+
+
+
+// let email = "cavid@code.edu.az";
+// let password = "00000000";
+
+// checkedEmail(email,password);
+
+// function checkedEmail(email, password) {
+//     if (email == "cavid@code.edu.az" && password == "00000000") {
+//         console.log("Girish olundu");
+//     }
+//     else{
+//         console.log("Sehvdir");
+//     }
+
+// }
+
+
+
+
+// let nums = [9,2,5,7,1];
+
+// oddNumsSum(nums);
+
+// function oddNumsSum(number){
+//     let sum = 0;
+//     for (let i = 0; i < number.length; i++) {
+//        if (nums[i] % 2 == 1) {
+//         sum += number[i];
+//        }
+//     }
+//     console.log(sum);
+// }
+
+
+
+
+// let nums = [4,55,23,7,9,18];
+
+// evenNumsCount(nums);
+
+// function evenNumsCount(numbers) {
+//     let count = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] % 2 == 0) {
+//             count++;
+
+//         }
+
+//     } console.log(count);
+
+// }
